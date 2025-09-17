abd55d9578328bda3c92f395853d956a290bac13
Isabela <osowskiisabela@gmail.com>
2025-09-17 12:26:45 -0300

    sensor controller

--- app/src/main/java/br/edu/utfpr/apicultura/app/DTO/PropertyDTO.java
+++ app/src/main/java/br/edu/utfpr/apicultura/app/DTO/PropertyDTO.java
@@ -2,4 +2,10 @@ package br.edu.utfpr.apicultura.app.DTO;
 
 import java.util.List;
 
-public record PropertyDTO (String address, String number, String name, String description, List<HiveDTO> hives) {}
+public record PropertyDTO (
+        String address, 
+        String number, 
+        String name, 
+        String description, 
+        List<HiveDTO> hives
+) {}
